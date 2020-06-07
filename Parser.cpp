12130c689c6d26808f77a2913404405e196afe09
Alexey Harenko <alexey16052002@gmail.com>
2020-06-07 15:39:10 +0300

    Shunting-yard algorithm

--- Parser.cpp
+++ Parser.cpp
@@ -1,1 +1,57 @@
 #include "Parser.h"
+#include "Parser.h"
+
+bool Parser::is_num(string other)
+{
+	for (int i = 0; i < oper.size(); i++)
+	{
+		if (oper[i] == other) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool Parser::higher_precedence(string other)
+{
+	if (opStack.empty())
+	{
+		return true;
+	}
+	else if (other != oper[0] && other != oper[1] && opStack.top()!= oper[0] && opStack.top() != oper[1]) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void Parser::shuting_yard_algo()
+{
+	
+	int index = 0;
+	while (!expresion.empty()) {
+		if (is_num(expresion.front()))
+		{
+			tree.push(expresion.front());
+			expresion.pop();
+			index++;
+		}
+		else {
+			if (higher_precedence(expresion.front())) {
+				opStack.push(expresion.front());
+				expresion.pop();
+			}
+			else {
+				tree.push(opStack.top());
+				opStack.pop();
+				opStack.push(expresion.front());
+				expresion.pop();
+			}
+		}
+	}
+	while (!opStack.empty()) {
+		tree.push(opStack.top());
+		opStack.pop();
+	}
+}
--- Parser.h
+++ Parser.h
@@ -1,9 +1,33 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
 class Parser
 {
+	vector<string> oper={"*", "/", "+", "-"};
+	queue<string> expresion;
+	queue<string> tree;
+	unordered_map<string, double> values;
+	stack<string> opStack;
+	bool is_num(string other);
+	bool higher_precedence(string other);
+public:
+	void shuting_yard_algo();
 };
 
+struct TokenBase {
+	virtual ~TokenBase() {}
+};
+
+template<class T> class Token : public TokenBase {
+public:
+	Token(T t) : val(t) {}
+	T val;
+};
+
+typedef queue<TokenBase*> TokenQueue;
